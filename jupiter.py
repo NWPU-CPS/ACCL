1ad0ecf04bc39122c5942a5afcc483f4077b89b6
zihaoDeng <1372524436@qq.com>
2025-04-22 19:19:27 +0800

    update

--- jupiter.py
+++ jupiter.py
@@ -5,9 +5,12 @@ from engine.engine import JupiterEngine
 
 def cfgname():
     # cfgname="nyf.dssaccl.cuda@1.batch@16.lr@40.loss@infoNCEWeightedDirect.gridn@2.ne" # 92.8 - map96.39
-    
+
     # cfgname="parisf.dss.cuda@0.batch@16.lr@20.loss@infoNCE" # 81.39
-    # cfgname="parisf.dssaccl.cuda@1.batch@16.lr@20.loss@infoNCEWeightedDirect.k@8.ne" # 84.96
+
+    # cfgname="parisf.dssaccl.cuda@3.batch@16.lr@20.loss@infoNCEWeightedDirect.k@8.ne" # 84.96 ? 78.92
+    # cfgname="parisf.dssaccl.cuda@2.batch@16.lr@20.loss@infoNCEWeightedDirect.k@16.ne" 78.75
+    cfgname="parisf.dssaccl.cuda@1.batch@16.lr@100.loss@infoNCEWeightedDirect.ne"
 
     # cfgname="parisf.dssaccl.cuda@2.batch@16.lr@40.loss@infoNCEWeightedDirect.gridn@7.k@16.ne2" # 79.95 
     # cfgname="parisf.dssaccl.cuda@0.batch@16.lr@40.loss@infoNCEWeightedDirect.gridn@4.k@16.ne2" # 79.95 
